b9adf713125dc0184e0b83e75e55c0c2db1bef81
Simon-4 <52985243+Simon-4@users.noreply.github.com>
2021-12-14 01:01:38 -0300

    Declared the content that will be displayed

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+function App() {
   const title = 'Blog Post'
   const body = 'This is my blog post'
   const comments = [
@@ -21,24 +22,15 @@
       </ul>
     </div>
   )
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>{title.toUpperCase()}</h1>
+      <p>{body}</p>
+
+      {showComments && commentBlock}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App

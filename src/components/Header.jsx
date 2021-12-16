2744dd6d655a42e08b639d7cbfb8c4435536d7ea
Simon-4 <52985243+Simon-4@users.noreply.github.com>
2021-12-16 01:28:36 -0300

    Created component directory

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+// Using 'dsznajder.es7-react-js-snippets' extension for snippets
+// Avoiding import React in every created component, just use undercoscore before the snippet shortcut
+// like instec 'rfce' use '_rfce'
+
+function Header({ text }) {
+  return (
+    <header>
+      <div className="className">
+        <h2>{text}</h2>
+      </div>
+    </header>
+  )
+}
+
+export default Header
